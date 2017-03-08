d0f339c511e85ff567d17e9f242e032a22b4287a
mvpratt <mvpratt@gmail.com>
2017-03-08 11:20:07 -0700

    Added ability to set doctor and patient addresses, and update states.  All tests pass.

--- contracts/ARX_Prescription.sol
+++ contracts/ARX_Prescription.sol
@@ -22,43 +22,50 @@ contract ARX_Prescription {
 
 	// Medication info
 	uint medication_id;   // ID in a shared registry of medications
-	uint refills_allowed;
 	uint refills_taken;
 
 	// Prescription state
-	uint constant CREATED = 0;
-	uint constant SIGNED = 1;
-	uint constant SUBMITTED = 2;
-	uint constant FILLED = 3;
-	uint constant EXPIRED = 4;
-	uint constant ERROR = 4;
-
+	uint constant CREATED = 0;   // doctor
+	uint constant SIGNED = 1;    // doctor
+	uint constant SUBMITTED = 2; // patient
+	uint constant FILLED = 3;    // pharmacy
+	uint constant EXPIRED = 4;   // pharmacy
+	uint constant ERROR = 4;     // self
 	uint rxstate;
+
+	uint constant MAX_REFILLS = 3;
+
+
+    // Define events
+    event OnDeployed(address indexed deployed);
+
+    event maxRefillsReached();
+
 
 	// Constructor function
 	function ARX_Prescription() {
 
         creator 		= msg.sender;
         owner 			= msg.sender;
-
+        doctor 			= 0;
+        patient 		= 0;
+        pharmacy 		= 0;
 		medication_id 	= 0;
-		refills_allowed = 0;
 		refills_taken 	= 0;
-
 		rxstate 		= CREATED;
 	}
 
-	function testDeploy() returns (address a){
-		a = new Test();
-		return a;
+	function testDeploy() {
+		address deployed = new Test();
+		OnDeployed(deployed);
+		return;
 	}
 
 
 // Prescription state functions
 
-	function setPrescriptionState(uint state) returns(bool) {
+	function setPrescriptionState(uint state) {
 		rxstate = state;
-        return true;
 	}
 	
 	function getPrescriptionState() returns(uint) {
@@ -67,57 +74,51 @@ contract ARX_Prescription {
 
 // Medication functions
 
-	function setMedicationID(uint num) returns(bool) {
+	function setMedicationID(uint num) {
 		medication_id = num;
-        return true;
 	}
 	
 	function getMedicationID() returns(uint) {
 		return medication_id;
 	}
-
-	function setRefillsAllowed(uint num) returns(bool) {
-		refills_allowed = num;
-        return true;
+	
+	function getRefillsLeft() returns(uint) {
+		return MAX_REFILLS - refills_taken;
 	}
 	
-	function getRefillsAllowed() returns(uint) {
-		return refills_allowed;
+	function incRefillsTaken() {
+		if (refills_taken == MAX_REFILLS-1) {
+		    refills_taken = MAX_REFILLS;
+		    rxstate = EXPIRED;
+        	maxRefillsReached();
+        }
+        else if (refills_taken < MAX_REFILLS) {
+        	refills_taken = refills_taken + 1;
+        }
 	}
 	
-	function incRefillsTaken() returns (bool) {
-		refills_taken = refills_taken + 1;
-        return true;
-	}
-	
-	function getRefillsTaken() returns(uint) {
-		return refills_taken;
-	}	
 
 
 // Stakeholder functions
 
-	function setPatient(address addr) returns(bool) {
+	function setPatient(address addr) {
 		patient = addr;
-        return true;
 	}
 	
 	function getPatient() returns(address) {
 		return patient;
 	}
 
-	function setDoctor(address addr) returns(bool) {
+	function setDoctor(address addr) {
 		doctor = addr;
-        return true;
 	}
 	
 	function getDoctor() returns(address) {
 		return doctor;
 	}
 
-	function setPharmacy(address addr) returns(bool) {
+	function setPharmacy(address addr) {
 		pharmacy = addr;
-        return true;
 	}
 	
 	function getPharmacy() returns(address) {
--- test/test_rx.js
+++ test/test_rx.js
@@ -1,9 +1,4 @@
-/* TODO: Add tests for
-  medication id
-  pharmacy
-  Test Stakeholders
-    Add test for doctor, pharmacy address, prescription state
-*/
+
 
 // accounts[0] = doctor
 // accounts[1] = patient
@@ -12,61 +7,178 @@
 
 contract('ARX_Prescription', function(accounts) {
 
-// -------- Doctor ---------------//
+  // Prescription state
+  const CREATED = 0;   
+  const SIGNED = 1;    
+  const SUBMITTED = 2; 
+  const FILLED = 3;    
+  const EXPIRED = 4;   
+  const ERROR = 4;     
 
-// Set medication ID
+
+it("Doctor sets doctor address", function() {
+  arx = ARX_Prescription.deployed();
+  arx.setDoctor(accounts[0], {from: accounts[0]});
+
+  return arx.getDoctor.call().then(function(addr) {
+      assert.equal(addr, accounts[0], "error: doctor address incorrect");
+    });
+});
+
+
 it("Doctor sets Medication ID to 42", function() {
   arx = ARX_Prescription.deployed();
-  arx.setMedicationID(42);
+  arx.setMedicationID(42, {from: accounts[0]});
 
   return arx.getMedicationID.call().then(function(id) {
       assert.equal(id.valueOf(), 42, "error: med id not 42");
     });
 });
 
-// Doctor sends Rx to Patient
-// send RX to new owner
+
+it("Doctor sets Patient address", function() {
+  arx = ARX_Prescription.deployed();
+  arx.setPatient(accounts[1], {from: accounts[0]});
+
+  return arx.getPatient.call().then(function(addr) {
+      assert.equal(addr, accounts[1], "error: patient address incorrect");
+    });
+});
+
+
+it("Doctor changes prescription state to SIGNED", function() {
+  arx = ARX_Prescription.deployed();
+  arx.setPrescriptionState(SIGNED, {from: accounts[0]});
+
+  return arx.getPrescriptionState.call().then(function(state) {
+      assert.equal(state, SIGNED, "error: Rx state not SIGNED");
+    });
+});
+
+
+
 it("Doctor transfers Rx to Patient",function(){
   arx = ARX_Prescription.deployed();
   
   return arx.transferOwner(accounts[1], {from: accounts[0]}).then(function(success){
     arx.getOwner.call().then(function(own) {
       assert.equal(own, accounts[1], "error: ownership not xferred");
-      //console.log("new owner: " + own)
     });
   });
 });  
 
-// Patient sends Rx to Pharmacy
-// send RX to new owner
-it("Patient sends Rx to Pharmacy",function(){
-  arx = ARX_Prescription.deployed();
-  
-  return arx.transferOwner(accounts[2], {from: accounts[1]}).then(function(success){
-    arx.getOwner.call().then(function(own) {
-      assert.equal(own, accounts[2], "error: ownership not xferred");
-      //console.log("new owner: " + own)
-    });
-  });
-});  
-
-// ------- Negative testing ------------//
-
-console.log("Negative testing")
-// Doctor tries to send RX to Pharmacy -- should fail
-// Refject Invalid RX transfer
-it("Doctor tries to transfer Rx back to Patient",function(){
+ 
+// Doctor tries to send RX back to patient -- should fail
+it("Negative testing:   Doctor tries to transfer Rx back to Patient",function(){
   arx = ARX_Prescription.deployed();
   
   return arx.transferOwner(accounts[1], {from: accounts[0]}).then(function(success){
     if (success != true){
-      //assert.equal(own, accounts[1], "error: ownership xferred but it shouldn't have been");
-      //console.log("success: ownership transfer rejected because sender is not owner");
     }
     else {
       console.log("error: ownership xferred without owner permission - should have failed");
     }
   });
 }); 
+
+
+it("Patient sets Pharmacy address",function(){
+  arx = ARX_Prescription.deployed();
+  
+  return arx.setPharmacy(accounts[2], {from: accounts[1]}).then(function(){
+    arx.getPharmacy.call().then(function(pharm) {
+      assert.equal(pharm, accounts[2], "error: pharmacy address incorrect");
+    });
+  });
+}); 
+
+
+it("Patient changes prescription state to SUBMITTED", function() {
+  arx = ARX_Prescription.deployed();
+  arx.setPrescriptionState(SUBMITTED, {from: accounts[0]});
+
+  return arx.getPrescriptionState.call().then(function(state) {
+      assert.equal(state, SUBMITTED, "error: Rx state not SUBMITTED");
+    });
+});
+
+
+it("Patient changes ownership to Pharmacy",function(){
+  arx = ARX_Prescription.deployed();
+  
+  return arx.transferOwner(accounts[2], {from: accounts[1]}).then(function(){
+    arx.getOwner.call().then(function(own) {
+      assert.equal(own, accounts[2], "error: ownership not xferred");
+    });
+  });
+}); 
+
+
+it("Patient requests a refill",function(){
+  arx = ARX_Prescription.deployed();
+  
+  return arx.incRefillsTaken({from: accounts[1]}).then(function(){
+    arx.getRefillsLeft.call().then(function(refill) {
+      assert.equal(refill, 2, "error: refills left not decremented");
+    });
+  });
+}); 
+
+
+it("Pharmacy changes prescription state to FILLED", function() {
+  arx = ARX_Prescription.deployed();
+  arx.setPrescriptionState(FILLED, {from: accounts[0]});
+
+  return arx.getPrescriptionState.call().then(function(state) {
+      assert.equal(state, FILLED, "error: Rx state not FILLED");
+    });
+});
+
+
+it("Patient requests 2nd refill",function(){
+  arx = ARX_Prescription.deployed();
+  
+  return arx.incRefillsTaken({from: accounts[1]}).then(function(){
+    arx.getRefillsLeft.call().then(function(refill) {
+      assert.equal(refill, 1, "error: refills left not decremented");
+    });
+  });
+}); 
+
+
+it("Patient requests last refill",function(){
+  arx = ARX_Prescription.deployed();
+  
+  return arx.incRefillsTaken({from: accounts[1]}).then(function(){
+    arx.getRefillsLeft.call().then(function(refill) {
+      assert.equal(refill, 0, "error: refills left not decremented");
+    });
+  });
+}); 
+
+
+it("Prescription state should change to EXPIRED automatically when 3rd refill is requested", function() {
+  arx = ARX_Prescription.deployed();
+
+  return arx.getPrescriptionState.call().then(function(state) {
+      assert.equal(state, EXPIRED, "error: Rx state not EXPIRED");
+    });
+});
+
+
+// Patient requests 4th refill -- should fail
+it("Negative Testing: Patient requests extra refill - should fail",function(){
+  arx = ARX_Prescription.deployed();
+
+  return arx.incRefillsTaken({from: accounts[1]}).then(function(){
+      arx.getRefillsLeft.call().then(function(refill) {
+      assert.equal(refill, 0, "error: max refills reached, refills left should be 0");
+    });
+  });
+});
+
+
+
+
 
 });
